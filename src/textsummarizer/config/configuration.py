7f28a700788808b543a2c4a11f0e2a4041bbc115
Chakshu Gupta <chakshugupta774@gmail.com>
2024-11-05 20:24:45 +0530

    data validation completed

--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -1,6 +1,7 @@
 from textsummarizer.constants import *
 from textsummarizer.utils.common import read_yaml, create_directories 
-from textsummarizer.entity import (DataIngestionConfig)
+from textsummarizer.entity import (DataIngestionConfig, DataValidationConfig)
+
 
 class ConfigurationManager:
     def __init__(
@@ -28,3 +29,18 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+        
+    def get_data_validation_config(self)->DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            status_file = config.status_file,
+            all_required_files = config.all_required_files
+        )
+
+        return data_validation_config
